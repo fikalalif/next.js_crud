d8bd6c7504e7019edb24488a90ba9804b4661ca5
fikalalif <fikalgans26@gmail.com>
2025-08-02 14:56:33 +0700

    success: fetch data

--- components/contact-table.tsx
+++ components/contact-table.tsx
@@ -1,29 +1,34 @@
+import { getContacts } from "@/lib/data";
+import React from "react";
+import { Contact } from '../app/generated/prisma/index';
+import { Prisma } from "@prisma/client";
 
-import React from 'react'
-
-const ContactTable = () => {
+const ContactTable = async () => {
+  const contacts = await getContacts();
   return (
-    <table className='w-full text-sm texl-left text-gray-500'>
-        <thead className='text-sm text-gray-700 uppercase bg-gray-50'>
-            <tr>
-                <th className='py-3 px-6'>#</th>
-                <th className='py-3 px-6'>name</th>
-                <th className='py-3 px-6'>Phone Number</th>
-                <th className='py-3 px-6'>Created At</th>
-                <th className='py-3 px-6 text-center'>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td className='py-3 px-6'></td>
-                <td className='py-3 px-6'></td>
-                <td className='py-3 px-6'></td>
-                <td className='py-3 px-6'></td>
-                <td></td>
-            </tr>
-        </tbody>
+    <table className="w-full text-sm texl-left text-gray-500">
+      <thead className="text-sm text-gray-700 uppercase bg-gray-50">
+        <tr>
+          <th className="py-3 px-6">#</th>
+          <th className="py-3 px-6">name</th>
+          <th className="py-3 px-6">Phone Number</th>
+          <th className="py-3 px-6">Created At</th>
+          <th className="py-3 px-6 text-center">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {contacts.map((contact, index) => (
+          <tr key={contact.id} className="bg-amber-50 border-b">
+            <td className="py-3 px-6">{index + 1}</td>
+            <td className="py-3 px-6">{contact.name}</td>
+            <td className="py-3 px-6">{contact.phone}</td>
+            <td className="py-3 px-6">{contact.createdAt.toString()}</td>
+            <td></td>
+          </tr>
+        ))}
+      </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default ContactTable
+export default ContactTable;
