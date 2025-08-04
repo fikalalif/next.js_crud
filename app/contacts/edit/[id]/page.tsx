d6d2a5c41991bec83459b34829c938c9cbab52f2
fikalalif <fikalgans26@gmail.com>
2025-08-05 02:21:40 +0700

    add: edit-form

---
+++ app/contacts/edit/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UpdateContactPage = () => {
+  return (
+    <div className='max-w-md mx-auto mt-5'>
+        <h1 className='text-2xl text-center mb-2'>
+            
+            
+        </h1>
+    </div>
+  )
+}
+
+export default UpdateContactPage
---
+++ components/edit-form.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import { saveContact } from "@/lib/actions";
+// import { useFormState } from "react-dom";
+import { useActionState } from "react";
+import { SubmitButton } from "./buttons";
+
+const CreateForm = () => {
+    const [state, formAction] = useActionState(saveContact,null);
+  return (
+    <div>
+        <form action={formAction}>
+            <div className="mb-5">
+                <label 
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-900"
+                >
+                    full name
+                </label>
+                <input 
+                type="text" 
+                name="name"
+                id="name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm
+                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                placeholder="full name.."
+                />
+                <div id="name-error" aria-live="polite" aria-atomic = "true" >
+                    <p className="mt-2 text-sm text-red-500"> {state?.Error?.name}</p>
+                </div>
+            </div>
+
+            <div className="mb-5">
+                <label 
+                htmlFor="phone"
+                className="block text-sm font-medium text-gray-900"
+                >
+                    full name
+                </label>
+                <input 
+                type="text" 
+                name="phone"
+                id="phone"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm
+                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                placeholder="phone number.."
+                />
+                <div id="phone-error" aria-live="polite" aria-atomic = "true" >
+                    <p className="mt-2 text-sm text-red-500"> {state?.Error?.phone}</p>
+                </div>
+            </div>
+
+            <div id="message-error" aria-live="polite" aria-atomic = "true" >
+                <p className="mt-2 text-sm text-red-500"> {state?.message}</p>
+            </div>
+
+            <SubmitButton label= "save"/>
+        </form>
+    </div>
+  )
+}
+
+export default CreateForm;
