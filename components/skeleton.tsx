b72521b125f524e531d538202571cc32a29a7b65
fikalalif <fikalgans26@gmail.com>
2025-08-12 21:57:22 +0700

    add: 5 row skeleton

--- components/skeleton.tsx
+++ components/skeleton.tsx
@@ -30,6 +30,82 @@ export const TableSkeleton = () => {
                         <div className="h-7 w-7 rounded-sm bg-gray-100"></div>
                     </td>
                   </tr>
+
+                  <tr className="bg-amber-50 border-b">
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-4 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-32 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-20 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-32 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="flex justify-center gap-1 py-3">
+                        <div className="h-7 w-7 rounded-sm bg-gray-100"></div>
+                        <div className="h-7 w-7 rounded-sm bg-gray-100"></div>
+                    </td>
+                  </tr>
+
+                  <tr className="bg-amber-50 border-b">
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-4 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-32 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-20 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-32 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="flex justify-center gap-1 py-3">
+                        <div className="h-7 w-7 rounded-sm bg-gray-100"></div>
+                        <div className="h-7 w-7 rounded-sm bg-gray-100"></div>
+                    </td>
+                  </tr>
+
+                  <tr className="bg-amber-50 border-b">
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-4 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-32 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-20 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-32 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="flex justify-center gap-1 py-3">
+                        <div className="h-7 w-7 rounded-sm bg-gray-100"></div>
+                        <div className="h-7 w-7 rounded-sm bg-gray-100"></div>
+                    </td>
+                  </tr>
+
+                  <tr className="bg-amber-50 border-b">
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-4 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-32 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-20 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="py-3 px-6">
+                        <div className="h-4 w-32 rounded bg-gray-100"></div>
+                    </td>
+                    <td className="flex justify-center gap-1 py-3">
+                        <div className="h-7 w-7 rounded-sm bg-gray-100"></div>
+                        <div className="h-7 w-7 rounded-sm bg-gray-100"></div>
+                    </td>
+                  </tr>
                 
               </tbody>
             </table>
